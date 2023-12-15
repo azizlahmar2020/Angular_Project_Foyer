900d4a1cb5fbf78b9960e662083892f6ae91ee77
RayenJouida <125547177+RayenJouida@users.noreply.github.com>
2023-12-15 08:53:56 +0100

    ppp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-
 
 @Component({
   selector: 'app-root',
@@ -10,7 +7,4 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'miniProjet';
-  constructor(private http: HttpClient) {}
-
-
 }
